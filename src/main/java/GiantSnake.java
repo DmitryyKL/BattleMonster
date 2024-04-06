3f7eb71d96aecbfe0a1aacdb8f94391372b60a8d
Dmitry.KL <d.klokov.kl@gmail.com>
2024-04-06 12:44:25 +0500

    2 version Monster

---
+++ src/main/java/GiantSnake.java
@@ -0,0 +1,32 @@
+public class GiantSnake extends Monster{
+    public static String scream = "Ssssss";
+
+    public GiantSnake(String name, int damage) {
+        super(name + " класса Гигантская змея", damage);
+    }
+
+    @Override
+    public void growl(){
+        super.growl();
+        System.out.println(scream);
+    }
+
+    public void growl(boolean loud){
+        if (!loud) {
+            growl();
+        }
+        else {
+            super.growl();
+            System.out.println(scream.toUpperCase());
+        }
+    }
+
+    @Override
+    public void attack(){
+        growl(true);
+        super.attack();
+        System.out.println("     ...and hid in the grass");
+    }
+
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Zombie zombie1 = new Zombie("Alice", 5);
-        zombie1.growl();
-        zombie1.growl(true);
-        zombie1.growl(false);
-        zombie1.attack();
+        Battle battle = new Battle();
+        battle.add(new Zombie("Alice", 5));
+        battle.add(new Zombie("Bob", 8));
+        battle.add(new Zombie("Eve", 3));
+        battle.add(new GiantSnake("Kaa",10));
+        battle.add(new GiantSnake("Son of Kaa", 12));
+
+        battle.add(new Zombie("Noname", 6));
+        battle.add(new GiantSnake("Noname",27));
+
+        battle.start();
     }
 }
