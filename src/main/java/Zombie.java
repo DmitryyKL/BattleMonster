0f01c27ede3288ba92629d385f91c3922596961d
Dmitry.KL <d.klokov.kl@gmail.com>
2024-04-06 18:31:49 +0500

    Version 3

--- src/main/java/Zombie.java
+++ src/main/java/Zombie.java
@@ -24,8 +24,8 @@ public class Zombie extends Monster{
     }
 
     @Override
-    public void attack(){
-        super.attack();
+    public void attack(Monster monster){
+       monster.damage(getDamage());
         growl();
     }
 
